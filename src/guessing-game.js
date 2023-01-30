e09bf11aff5ac1c12ffc77bf8282a3bbf99312d6
TatarchukVladimir <tatarchuk.vladimir16@gmail.com>
2023-01-30 18:41:51 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,19 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        return this.max = Math.ceil((this.max + this.min) / 2);
     }
 
     greater() {
-
+        return this.min = Math.ceil((this.max + this.min) / 2);
     }
 }
 
